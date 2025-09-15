75bac24b9cfb5380d54928ef667a471088e74655
PreetiKudiwal <preetikudiwal@gmail.com>
2025-09-15 13:29:58 +0530

    Connects mongodb atlas

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5001;
 server.use(express.json());
 server.use(cors(
     {
-        origin: "*",
+        origin: process.env.FRONTEND_URL,
         methods: ["GET", "POST", "PUT", "DELETE"],
         allowedHeaders: ["Content-Type", "Authorization"],
         credentials: true
